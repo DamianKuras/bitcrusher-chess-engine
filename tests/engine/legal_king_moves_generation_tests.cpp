e6187431d4f7fd7ecb75dfc9d2f0f88d0ac44580
DamianKuras <damian_kuras@outlook.com>
2025-04-05 23:52:51 +0200

    test: add legal knight and king move generation tests

---
+++ tests/engine/legal_king_moves_generation_tests.cpp
@@ -0,0 +1,71 @@
+#include "bitboard_enums.hpp"
+#include "board_state.hpp"
+
+#include "legal_moves_generator.hpp"
+#include "move_generation_fixture.hpp"
+#include "test_helpers.hpp"
+#include <gtest/gtest.h>
+
+using bitcrusher::BoardState;
+using bitcrusher::Color;
+using test_helpers::LegalMoveGenerationParametrizedTest;
+using test_helpers::LegalMovesTestCase;
+using test_helpers::TestMoveSink;
+
+namespace {
+void generateLegalKingMovesWhite(const BoardState& board, TestMoveSink& sink) {
+    bitcrusher::generateLegalKingMoves<TestMoveSink, Color::WHITE>(board, sink);
+}
+
+void generateLegalKingMovesBlack(const BoardState& board, TestMoveSink& sink) {
+    bitcrusher::generateLegalKingMoves<TestMoveSink, Color::BLACK>(board, sink);
+}
+} // namespace
+
+const std::array<LegalMovesTestCase, 8> LEGAL_KING_MOVES_TEST_CASES{{
+    {.name = "king in the center of the board unblocked and not attacked should have 8 legal moves",
+     .fen  = "k7/8/8/3K4/8/8/8/8 w - - 0 1",
+     .expected_moves = {"d5c4", "d5d4", "d5e4", "d5c5", "d5e5", "d5c6", "d5d6", "d5e6"},
+     .move_generator = generateLegalKingMovesWhite},
+
+    {.name           = "king should be able to capture enemy piece",
+     .fen            = "k7/8/8/2pKp3/8/8/8/8 w - - 0 1",
+     .expected_moves = {"d5c4", "d5e4", "d5c5", "d5e5", "d5c6", "d5d6", "d5e6"},
+     .move_generator = generateLegalKingMovesWhite},
+
+    {.name           = "king should not be able to move to square occupied by friendly piece",
+     .fen            = "k7/8/2PPP3/2PKP3/2PPP3/8/8/8 w - - 0 1",
+     .expected_moves = {},
+     .move_generator = generateLegalKingMovesWhite},
+
+    {.name = "king in position where kingside castle is allowed should be able to castle kingside",
+     .fen  = "k7/8/8/8/8/8/8/4K2R w K - 0 1",
+     .expected_moves = {"e1d1", "e1d2", "e1e2", "e1f2", "e1f1", /*white kingside castle*/ "e1g1"},
+     .move_generator = generateLegalKingMovesWhite},
+
+    {.name =
+         "king in position where queenside castle is allowed should be able to castle queenside",
+     .fen            = "7k/8/8/8/8/8/8/R3K3 w Q - 0 1",
+     .expected_moves = {"e1d1", "e1d2", "e1e2", "e1f2", "e1f1", /* white queenside castle*/ "e1c1"},
+     .move_generator = generateLegalKingMovesWhite},
+
+    {.name           = "king should not be able to castle under check",
+     .fen            = "k3r3/8/8/8/8/8/8/4K2R w K - 0 1",
+     .expected_moves = {"e1d1", "e1d2", "e1f1", "e1f2"}, // only evasions moves are legal
+     .move_generator = generateLegalKingMovesWhite},
+
+    {.name = "king should not be able to castle when square between king and rook is occupied",
+     .fen  = "k7/8/8/8/8/8/8/4KN1R w K - 0 1",
+     .expected_moves = {"e1d1", "e1d2", "e1e2", "e1f2"},
+     .move_generator = generateLegalKingMovesWhite},
+
+    {.name           = "king should be able to capture checking piece",
+     .fen            = "k7/8/8/8/8/8/4r3/4K3 w a - 0 1",
+     .expected_moves = {"e1d1", "e1f1", /* capturing rook that is giving a check */ "e1e2"},
+     .move_generator = generateLegalKingMovesWhite},
+
+}};
+
+INSTANTIATE_TEST_SUITE_P(KnightMovesTests,
+                         LegalMoveGenerationParametrizedTest,
+                         ::testing::ValuesIn(LEGAL_KING_MOVES_TEST_CASES));
---
+++ tests/engine/legal_knight_moves_generation_tests.cpp
@@ -0,0 +1,71 @@
+#include "bitboard_enums.hpp"
+#include "board_state.hpp"
+#include "legal_moves_generator.hpp"
+#include "move_generation_fixture.hpp"
+#include "test_helpers.hpp"
+#include <gtest/gtest.h>
+
+using bitcrusher::BoardState;
+using bitcrusher::Color;
+using bitcrusher::generateLegalKnightMoves;
+using test_helpers::LegalMoveGenerationParametrizedTest;
+using test_helpers::LegalMovesTestCase;
+using test_helpers::TestMoveSink;
+
+namespace {
+void generateLegalKnightMovesWhite(const BoardState& board, TestMoveSink& sink) {
+    generateLegalKnightMoves<TestMoveSink, Color::WHITE>(board, sink);
+}
+
+void generateLegalKnightMovesBlack(const BoardState& board, TestMoveSink& sink) {
+    generateLegalKnightMoves<TestMoveSink, Color::BLACK>(board, sink);
+}
+} // namespace
+
+
+const std::array<LegalMovesTestCase, 8> LEGAL_KNIGHT_MOVES_TEST_CASES{{
+    {.name           = "starting chess position white knight moves",
+     .fen            = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+     .expected_moves = {"b1a3", "b1c3", "g1f3", "g1h3"},
+     .move_generator = generateLegalKnightMovesWhite},
+
+    {.name           = "starting chess position black knight moves",
+     .fen            = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1",
+     .expected_moves = {"b8a6", "b8c6", "g8f6", "g8h6"},
+     .move_generator = generateLegalKnightMovesBlack},
+
+    {.name           = " knight in center should have 8 legal moves",
+     .fen            = "k7/8/8/8/3N4/8/8/K7 w - - 0 1",
+     .expected_moves = {"d4b5", "d4b3", "d4c6", "d4c2", "d4e6", "d4e2", "d4f5", "d4f3"},
+     .move_generator = generateLegalKnightMovesWhite},
+
+    {.name           = " knight blocked by friendly pieces should not be able to jump on them",
+     .fen            = "k7/8/2P1P3/8/3N4/8/8/K7 w - - 0 1",
+     .expected_moves = {"d4b5", "d4b3", "d4c2", "d4e2", "d4f5", "d4f3"},
+     .move_generator = generateLegalKnightMovesWhite},
+
+    {.name           = "knight in corner should have 2 legal moves",
+     .fen            = "7k/8/8/8/8/8/8/N6K w - - 0 1",
+     .expected_moves = {"a1b3", "a1c2"},
+     .move_generator = generateLegalKnightMovesWhite},
+
+    {.name           = "knight should be able to capture enemy pieces",
+     .fen            = "k7/8/8/5p2/3N4/1p6/8/K7 w - - 0 1",
+     .expected_moves = {"d4b5", "d4b3", "d4c6", "d4c2", "d4e6", "d4e2", "d4f5", "d4f3"},
+     .move_generator = generateLegalKnightMovesWhite},
+
+    {.name           = "knight pinned vertically should not be able to move",
+     .fen            = "3r3k/8/8/8/8/8/3N4/3K4 w - - 0 1",
+     .expected_moves = {},
+     .move_generator = generateLegalKnightMovesWhite},
+
+    {.name           = "knight pinned diagonally should not be able to move",
+     .fen            = "k7/8/7b/8/8/8/3N4/2K5 w - - 0 1",
+     .expected_moves = {},
+     .move_generator = generateLegalKnightMovesWhite},
+
+}};
+
+INSTANTIATE_TEST_SUITE_P(KnightMovesTests,
+                         LegalMoveGenerationParametrizedTest,
+                         ::testing::ValuesIn(LEGAL_KNIGHT_MOVES_TEST_CASES));
