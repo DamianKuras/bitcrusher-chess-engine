a74a3633d89e6be9bc5362c2548d10d293abb798
DamianKuras <damian_kuras@outlook.com>
2025-08-19 22:44:18 +0200

    feat: add EPD file parsing functionality

--- benchmarks/benchmark_helper_functions.cpp
+++ benchmarks/benchmark_helper_functions.cpp
@@ -1,10 +1,11 @@
 #include "benchmark_helper_functions.hpp"
 
 #include <fstream>
+#include <iostream>
 
 namespace bench::utils {
 
-std::string loadFenFromFile(const std::filesystem::path& file_path) {
+std::string loadFENFromFile(const std::filesystem::path& file_path) {
     std::ifstream file(file_path);
     if (! file.is_open()) {
         throw std::runtime_error("Error: Unable to open file: " + file_path.string());
@@ -14,6 +15,40 @@ std::string loadFenFromFile(const std::filesystem::path& file_path) {
         throw std::runtime_error("Error: Unable to read from file: " + file_path.string());
     }
     return fen;
+}
+
+namespace {
+Epd stringToEPD(const std::string& line) {
+    std::istringstream iss(line);
+    std::string        token;
+    int                field_count = 0;
+    Epd                epd;
+    while (iss >> token) {
+        if (field_count < 4) {
+            if (! epd.fen.empty()) {
+                epd.fen += " ";
+            }
+            epd.fen += token;
+            ++field_count;
+        } else if (token == "bm") {
+            iss >> epd.best_move;
+            epd.best_move.pop_back(); // remove ';' after best move
+        }
+    }
+    return epd;
+}
+} // namespace
+
+[[nodiscard]] Epd loadEPDFromFile(const std::filesystem::path& file_path) {
+    std::ifstream file(file_path);
+    if (! file.is_open()) {
+        throw std::runtime_error("Error: Unable to open file: " + file_path.string());
+    }
+    std::string line;
+    if (! std::getline(file, line)) {
+        throw std::runtime_error("Error: Unable to read from file: " + file_path.string());
+    }
+    return stringToEPD(line);
 }
 
 } // namespace bench::utils
--- benchmarks/benchmark_helper_functions.hpp
+++ benchmarks/benchmark_helper_functions.hpp
@@ -6,10 +6,21 @@
 
 namespace bench::utils {
 
-// Load the first line from a given file. Used for loading single fen from files.
+struct Epd {
+    std::string fen;
+    std::string best_move;
+};
+
+// Load the first line from a given file. Used for loading single Forsyth-Edwards Notation (FEN)
+// from file.
 //
 // throws std::runtime_error if the file cannot be opened or read
-[[nodiscard]] std::string loadFenFromFile(const std::filesystem::path& file_path);
+[[nodiscard]] std::string loadFENFromFile(const std::filesystem::path& file_path);
 
+// Load the first line from a given file. Used for loading single Extended Position Description
+// (EPD) from file.
+//
+// throws std::runtime_error if the file cannot be opened or read
+[[nodiscard]] Epd loadEPDFromFile(const std::filesystem::path& file_path);
 } // namespace bench::utils
 #endif // BENCHMARK_HELPER_FUNCTIONS_HPP
